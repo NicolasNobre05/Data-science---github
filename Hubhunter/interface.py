98b422a79ceca63ca838a2e6371691b656251e9f
Nicolas Nobre <165395180+NicolasNobre05@users.noreply.github.com>
2024-06-10 16:56:36 -0300

    Update interface.py

--- Hubhunter/interface.py
+++ Hubhunter/interface.py
@@ -2,44 +2,84 @@ import customtkinter
 import webbrowser
 from hubhunter import consultardados
 import re
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
 
 def is_url(text):
-    # Verifica se o texto corresponde a um padrão de URL
     url_pattern = re.compile(r'https?://\S+')
     return url_pattern.match(text) is not None
 
-def on_consultar():
-    # Recupera os dados dos campos de entrada
+def load_image_from_url(url, size=(60, 60), border_color="#144673", border_width=2):
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    image = image.resize(size, Image.LANCZOS)
+    
+    # Adicionando uma borda à imagem
+    border_image = Image.new("RGB", (size[0] + 2 * border_width, size[1] + 2 * border_width), border_color)
+    border_image.paste(image, (border_width, border_width))
+    
+    return ImageTk.PhotoImage(border_image)
+
+
+def on_consultar(contpag=1):
+    global resultados_frame
+
     nome = nome_entry.get()
     linguagem = linguagem_entry.get()
     localidade = localidade_entry.get()
 
-    # Chama a função de processamento com os dados
-    resultados = consultardados(nome, linguagem, localidade)
-
-    # Limpa o conteúdo anterior da caixa de texto de resultados
-    resultados_textbox.delete("1.0", "end")
+    resultados = consultardados(nome, linguagem, localidade, contpag)
+    
+    for widget in resultados_frame.winfo_children():
+        widget.destroy()
 
     if resultados is not None:
-        # Adiciona os novos resultados na caixa de texto
         for resultado in resultados:
-            # Verifica se o texto é uma URL antes de adicionar a tag de link
-            if is_url(resultado):
-                resultados_textbox.insert("end", resultado + "\n", "link")
-            else:
-                resultados_textbox.insert("end", resultado + "\n")
+            url, image_url = resultado.split(", ")
+            frame = customtkinter.CTkFrame(master=resultados_frame, fg_color="#D9D9D9")
+            frame.pack(fill="x", pady=5)
+
+            try:
+                image = load_image_from_url(image_url)
+                image_label = customtkinter.CTkLabel(master=frame, image=image, text="")
+                image_label.image = image 
+                image_label.pack(side="left", padx=10)
+            except Exception as e:
+                print(f"Erro ao carregar a imagem: {e}")
+
+            text_label = customtkinter.CTkLabel(master=frame, text=url, fg_color="#D9D9D9", text_color="blue", cursor="hand2")
+            text_label.pack(side="left", padx=10)
+            text_label.bind("<Button-1>", lambda e, url=url: abrir_url(url))
+        
+        next_page_button.place(relx=1.0, rely=1.0, x=-100, y=-10, anchor='se')
     else:
-        resultados_textbox.insert("end", "Nenhum resultado encontrado.\n")
+        no_results_label = customtkinter.CTkLabel(master=resultados_frame, text="Nenhum resultado encontrado.", fg_color="#D9D9D9")
+        no_results_label.pack()
+
+    canvas.yview_moveto(0)
+
 
 def abrir_url(url):
-    # Abre a URL no navegador padrão
     webbrowser.open_new_tab(url)
 
+def on_next_page():
+    global current_page
+    current_page += 1
+    on_consultar(current_page)
+    canvas.yview_moveto(0)
+
+
+def on_mouse_wheel(event):
+    canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+
 def criar_janela():
+    global nome_entry, linguagem_entry, localidade_entry, resultados_frame, next_page_button, current_page, canvas
+
     janela = customtkinter.CTk()
     janela.title("interface")
     janela.geometry("1400x700")
-
+    
     top_bar = customtkinter.CTkFrame(master=janela, fg_color="#0D1E40", width=1400, height=140, corner_radius=0)
     top_bar.pack_propagate(0)
     top_bar.pack(fill="x", anchor="n")
@@ -54,31 +94,45 @@ def criar_janela():
     grid_entradas = customtkinter.CTkFrame(master=frame_principal, fg_color="transparent")
     grid_entradas.pack(fill="x", padx=250, pady=(50,0), anchor="n")
 
-    global nome_entry
     nome_entry = customtkinter.CTkEntry(master=grid_entradas, placeholder_text="Nome completo", placeholder_text_color="#0D1E40", fg_color="transparent", border_color="#0D1E40", border_width=2, width=400, corner_radius=10, text_color="#000000")
     nome_entry.grid(row=0, column=0, ipady=10, sticky="n", pady=(24,0))
 
-    global linguagem_entry
     linguagem_entry = customtkinter.CTkEntry(master=grid_entradas, placeholder_text="Linguagem de programação", placeholder_text_color="#0D1E40", fg_color="transparent", border_color="#0D1E40", border_width=2, width=400, corner_radius=10, text_color="#000000")
     linguagem_entry.grid(row=1, column=0, ipady=10, sticky="n", pady=(24,0))
 
-    global localidade_entry
     localidade_entry = customtkinter.CTkEntry(master=grid_entradas, placeholder_text="Localidade", placeholder_text_color="#0D1E40", fg_color="transparent", border_color="#0D1E40", border_width=2, width=400, corner_radius=10, text_color="#000000")
     localidade_entry.grid(row=0, column=1, ipady=10, sticky="n", pady=(24,0), padx=(24,0))
 
-    consulta = customtkinter.CTkButton(master=grid_entradas, text="consultar", text_color="#D9D9D9", width=400, fg_color="#0D1E40", font=("Arial Bold", 17), hover_color="#144673", border_width=2, corner_radius=10, command=on_consultar)
+    consulta = customtkinter.CTkButton(master=grid_entradas, text="consultar", text_color="#D9D9D9", width=400, fg_color="#0D1E40", font=("Arial Bold", 17), hover_color="#144673", border_width=2, corner_radius=10, command=lambda: on_consultar(1))
     consulta.grid(row=1, column=1, ipady=10, sticky="n", pady=(24,0), padx=(24,0))
 
-    # Adiciona uma caixa de texto para exibir os resultados
-    global resultados_textbox
-    resultados_textbox = customtkinter.CTkTextbox(master=frame_principal, width=1000, height=300, corner_radius=10, fg_color="#D9D9D9", text_color="#000000")
-    resultados_textbox.pack(pady=20)
+    canvas = customtkinter.CTkCanvas(frame_principal)
+    canvas.config(width=1400, height=800, bg="#D9D9D9")  # Define a cor de fundo do canvas
 
-    # Adiciona uma tag de link clicável à caixa de texto
-    resultados_textbox.tag_config("link", foreground="blue", underline=True)
+    background_rect = canvas.create_rectangle(0, 0, 1400, 700, fill="#D9D9D9" ,outline="")
+    canvas.tag_lower(background_rect)  # Coloca o retângulo atrás de outros itens no canvas
 
-    # Liga o evento de clique à função abrir_url apenas para o texto que é uma URL
-    resultados_textbox.tag_bind("link", "<Button-1>", lambda event: abrir_url(event.widget.get("current linestart", "current lineend")))
+    scrollbar = customtkinter.CTkScrollbar(frame_principal, orientation="vertical", command=canvas.yview)
+    resultados_frame = customtkinter.CTkFrame(canvas, fg_color="#D9D9D9", bg_color="#D9D9D9", border_color="blue")
+
+    resultados_frame.bind(
+        "<Configure>",
+        lambda e: canvas.configure(
+            scrollregion=canvas.bbox("all")
+        )
+    )
+
+    canvas.create_window((0, 0), window=resultados_frame, anchor="nw", width=1400, height=800)
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind_all("<MouseWheel>", on_mouse_wheel)
+
+    canvas.pack(side="left", fill="both", expand=True)
+    scrollbar.pack(side="right", fill="y")
+
+    next_page_button = customtkinter.CTkButton(master=frame_principal, text="Próxima Página", text_color="#D9D9D9", width=200, fg_color="#0D1E40", font=("Arial Bold", 17), hover_color="#144673", border_width=2, corner_radius=10, command=on_next_page)
+    next_page_button.place(relx=1.0, rely=1.0, x=-100, y=-10, anchor='se')
+
+    current_page = 1
 
     janela.mainloop()
 
